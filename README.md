7402edde6532e750afe3fc3d4dd8082b3832b07a
Brian Siaotickchong <bsiaotickchong@gmail.com>
2016-11-13 15:45:14 -0500

    Updated jobs.js to properly get the search term from index.html, and the /jobs route now gives jobs.jade the entire array of job dictionaries. Updated jobs.jade to render the entire array of job dictionaries. Updated README to explain how to start the node.js server.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Blazr
 This repository includes all files pertaining to the Blazr job finder app. Also includes all assignments relating to the Blazr app for CS 411 at Boston University.
+
+- run the node server (app.js) by using "npm start" in the repo's directory
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,13 +2,15 @@ var express = require('express');
 var router = express.Router();
 var request = require("request");
 
-router.get ('/', function (req, res, next) {
+router.post ('/', function (req, res, next) {
+	
+	console.log('Search term: '+req.body.searchterm)
+	console.log('Search term; '+req.query.searchterm)
 
     var options = { method: 'GET',
         url: 'https://jobs.github.com/positions.json',
         qs:{ 
-            search: req.query.searchterm,
-            page: '1',
+            description: req.body.searchterm,
         headers:
         { 'postman-token': 'ea90e0ac-7c80-e2a4-ab57-3025cc142381',
 			'cache-control': 'no-cache' }  } };
@@ -18,20 +20,22 @@ router.get ('/', function (req, res, next) {
 
         //console.log(body);
 
-        var searchterm = req.query.searchterm;
-		//console.log(req);
+        var searchterm = req.body.searchterm;
+		console.log(req);
         //res.send({searchterm: searchterm, body: body});
 		
 		var json_body = JSON.parse(body)
-		var titles = []
-		for (var i = 0; i < json_body.length; i++){
-			var curr_title = json_body[i].title;
-			titles.push(curr_title);
-			console.log(curr_title);
-		}
+		var jobs_results = json_body //give all the results to the jobs_results object
+		
+		/*for (var i = 0; i < json_body.length; i++){
+			//var job_title = json_body[i].title;
+			var job_created_at = json_body[i].created_at;
+			var job_location = json_body[i].location;
+			jobs_results = json_body[i]
+		}*/
 		
 		//res.send(titles);
-		res.render('jobs', { jobs: titles });
+		res.render('jobs', { jobs: jobs_results }); //give jobs.jade all of the results
     });
 });
 
