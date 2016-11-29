da9285200ba28b1e018543582b124203f32057d4
Brian Siaotickchong <bsiaotickchong@gmail.com>
2016-11-28 19:13:13 -0500

    Update directories and blazr.py

--- blazr.py
+++ blazr.py
@@ -1,14 +1,23 @@
 from json import load, dumps
-from flask import Flask, request, render_template, url_for, redirect, jsonify
+from flask import Flask, request, render_template, url_for, redirect, jsonify, send_from_directory
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/')
 jobs_url = "https://jobs.github.com/positions.json"
 
-@app.route('/')
-def index():
-	return render_template('index.html')
+#This route allows javascript to include javascript files by saying src="/scripts/filepathgoeshere"
+@app.route('/scripts/<path:path>')
+def send_js(path):
+    return send_from_directory('scripts', path)
 
+@app.route('/')
+def root():
+	return send_from_directory('views','index.html') #send_from_directory is safer
+
+@app.route('/home')
+def home():
+	return send_from_directory('views','home.html')
+	
 @app.route('/jobs', methods=['POST'])
 def jobs():
     searchterm=request.form['searchterm']
---
+++ scripts/controllers/MainController.js
@@ -0,0 +1,25 @@
+app.controller('MainController', ['$scope', function($scope) {
+	$scope.title = 'Top Jobs in Your Area:';
+	$scope.jobs = [
+		{
+			title: 'Software Engineer',
+			company: 'Apple',
+			deadline: new Date('2016', '12', '08'),
+			like: false,
+			dislike: false
+		},
+		{
+			title: 'CEO',
+			company: 'Google',
+			deadline: new Date('2016', '11', '28'),
+			like: false,
+			dislike: false
+		}
+	];
+	$scope.like = function(index) {
+		$scope.jobs[index].like = true;
+	};
+	$scope.dislike = function(index) {
+		$scope.jobs[index].dislike = true;
+	};
+}]);
