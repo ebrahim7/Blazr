af2b8674ab7d7d7c1b431f193c1bb075b6d274ef
ebrahim7 <ebrahim7@bu.edu>
2016-12-01 21:15:46 -0500

    Add files via upload
    template / rough version of the router uploaded

---
+++ control.py
@@ -0,0 +1,54 @@
+from json import load, dumps
+
+
+def login(data):
+	uname = data['uname']
+	password = data['password']
+	key = data['key']
+	return 'logged in'
+
+def create_account(data):
+	uname = data['uname']
+	password = data['password']
+	key = data['key']
+	return 'created'
+
+def get_jobs(term):
+    response = requests.get(jobs_url, params={'description': term})
+    return response.text
+
+def like_job(data):
+	# store job data in user DB
+	return 'liked'
+
+def app_edit_bio(data):
+	uname = data['uname']
+	bio = data['bio']
+	return 'success'
+
+def app_add_skill(data):
+	uname = data['uname']
+	skill = data['skill']
+	return 'success'
+
+def app_remove_skill(data):
+	uname = data['uname']
+	skill = data['skill']
+	return 'success'
+
+
+def app_remove_skill(data):
+	uname = data['uname']
+	skill = data['skill']
+	return 'success'
+	
+def app_add_experience(data):
+	uname = data['uname']
+	exp = data['experience']
+	return 'success'
+
+def app_remove_experience(data):
+	uname = data['uname']
+	exp = data['experience']
+	return 'success'
+
