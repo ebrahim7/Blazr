1c560c58afe4867242e8d3e62a32d0f45d1c8147
Kyle Pietz <kylepietz@dhcp-wifi-8021x-155-41-102-153.bu.edu>
2016-12-04 23:25:05 -0500

    update home, maincontroller, login, style, myprofile

--- scripts/controllers/MainController.js
+++ scripts/controllers/MainController.js
@@ -6,12 +6,22 @@ app.controller('MainController', ['$scope', function($scope) {
 	$scope.dislike = function(index) {
 		$scope.jobs[index].dislike = true;
 	};
-  
+
   $scope.toggle = [];
+	$scope.toggleMoreLess = [];
+
   $scope.jobDescToggle = function(index) {
     console.log('toggled: '+index +' to: '+!$scope.toggle[index])
     $scope.toggle[index] = !$scope.toggle[index];
+		$scope.toggleMoreLess[index] = !$scope.toggleMoreLess[index];
+
+
   };
-  
+	$scope.hide = [];
+	$scope.removeJob = function(index) {
+		$scope.hide[index] = true;
+		console.log("got function");
+	}
+
   console.log('MainController.js loaded')
 }]);
